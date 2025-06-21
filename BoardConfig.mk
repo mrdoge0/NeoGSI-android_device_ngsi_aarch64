b94f596353b07a360a082d6f42b78f639b0046fe
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2025-06-21 16:06:19 +0300

    Create BoardConfig.mk

---
+++ BoardConfig.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2025 The NeoGSI Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a73
+
+# Architecture 2
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a73
+
+# DON'T create extras!
+TARGET_NO_RECOVERY := true
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := true
+
+# RootFS
+BOARD_ROOT_EXTRA_FOLDERS += bt_firmware sec_storage efs persist
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BOARD_ROOT_EXTRA_SYMLINKS := $(filter-out $(BOARD_ROOT_EXTRA_SYMLINKS),/mnt/vendor/persist:/persist)
+
+# Image sizes
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648
