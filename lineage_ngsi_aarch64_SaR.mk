3bb5128d3f338d17dd17568c9cb10a1e0223cf18
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2025-06-21 16:32:54 +0300

    Create lineage_ngsi_aarch64_SaR.mk

---
+++ lineage_ngsi_aarch64_SaR.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2025 The NeoGSI Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit AOSP
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+$(call inherit-product-if-exists, build/target/product/embedded.mk)
+
+# Inherit LineageOS
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit NeoGSI common makefile
+$(call inherit-product, vendor/ngsi/implement.mk)
+
+# SaR (System-as-Root)
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+
+# Set initial device and target info
+PRODUCT_NAME := lineage_ngsi_aarch64_SaR
+PRODUCT_DEVICE := generic_arm64
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := AOSP on ARM64
