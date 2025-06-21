0b2c5e66d25d8a8432e514bded77d79ac6c30231
Mr. Doge <78793304+mrdoge0@users.noreply.github.com>
2025-06-21 16:46:16 +0300

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2025 The NeoGSI Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit NeoGSI ARM64 configs
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_ngsi_aarch64_SaR.mk \
+    $(LOCAL_DIR)/aosp_ngsi_aarch64_SaS.mk \
+    $(LOCAL_DIR)/lineage_ngsi_aarch64_SaR.mk \
+    $(LOCAL_DIR)/lineage_ngsi_aarch64_SaS.mk
